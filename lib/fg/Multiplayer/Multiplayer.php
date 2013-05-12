bbbfd27db7d9ce9f367c2bb940cb804f392e91b3
Félix Girault <felix.girault@gmail.com>
2013-05-12 22:00:09 +0200

    Dunno

--- lib/fg/Multiplayer/Multiplayer.php
+++ lib/fg/Multiplayer/Multiplayer.php
@@ -19,14 +19,15 @@ class Multiplayer {
 	 *
 	 */
 
-	protected $_settings = array(
+	protected $_options = array(
 		'autoPlay' => null,
 		'showInfos' => null,
 		'showBranding' => null,
 		'showRelated' => null,
 		'backgroundColor' => null,
 		'foregroundColor' => null,
-		'highlightColor' => null
+		'highlightColor' => null,
+		'wrapper' => '<iframe src="%s" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
 	);
 
 
@@ -39,7 +40,7 @@ class Multiplayer {
 		'dailymotion' => array(
 			'id' => '#dailymotion\\.com/(embed/)?video/(?<id>[a-z0-9]+)#i',
 			'player' => 'http://www.dailymotion.com/embed/video/%s',
-			'settings' => array(
+			'map' => array(
 				'autoPlay' => 'autoplay',
 				'showInfos' => 'info',
 				'showBranding' => 'logo',
@@ -61,7 +62,7 @@ class Multiplayer {
 		'vimeo' => array(
 			'id' => '#vimeo\\.com/(video/)?(?<id>[0-9]+)#i',
 			'player' => 'http://player.vimeo.com/video/%s',
-			'settings' => array(
+			'map' => array(
 				'autoPlay' => 'autoplay',
 				'showInfos' => array( 'byline', 'portrait' ),
 				'highlightColor' => 'color'
@@ -70,7 +71,7 @@ class Multiplayer {
 		'youtube' => array(
 			'id' => '#(v=|v/|embed/|youtu\\.be/)(?<id>[a-z0-9_-]+)#i',
 			'player' => 'http://www.youtube-nocookie.com/embed/%s',
-			'settings' => array(
+			'map' => array(
 				'autoPlay' => 'autoplay',
 				'showInfos' => 'showinfo',
 				'showRelated' => 'rel'
@@ -81,12 +82,13 @@ class Multiplayer {
 
 
 	/**
-	 *	HTML code for an <iframe> tag.
 	 *
-	 *	@var string
 	 */
 
-	protected $_iFrame = '<iframe src="%s" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+	public function __construct( array $providers = array( )) {
+
+		$this->_providers = array_merge( $this->_providers, $providers );
+	}
 
 
 
@@ -94,23 +96,29 @@ class Multiplayer {
 	 *	Prepares an HTML embed code.
 	 *
 	 *	@param string $source URL or HTML code.
+	 *	@param array $options
 	 *	@return string Prepared HTML code.
 	 */
 
-	public function html( $source ) {
+	public function html( $source, array $options = array( )) {
 
+		$options = array_merge( $this->_options, $options );
 		$html = '';
-		list( $provider, $id ) = $this->_providerInfos( $source );
 
-		if ( $provider ) {
-			$url = sprintf( $this->_providers[ $provider ]['player'], $id );
-			$params = $this->_params( $provider );
+		list( $providerName, $videoId ) = $this->_providerInfos( $source );
+
+		if ( $providerName ) {
+			$params = $this->_params( $providerName, $options );
+			$url = sprintf(
+				$this->_providers[ $providerName ]['player'],
+				$videoId
+			);
 
 			if ( $params ) {
 				$url .= '?' . http_build_query( $params );
 			}
 
-			$html .= sprintf( $this->_iFrame, $url );
+			$html .= sprintf( $options['wrapper'], $url );
 		} else {
 
 		}
@@ -141,25 +149,25 @@ class Multiplayer {
 	 *
 	 */
 
-	protected function _params( $provider ) {
+	protected function _params( $provider, $options ) {
 
 		$params = array( );
 
-		foreach ( $this->_providers[ $provider ]['settings'] as $key => $value ) {
-			if ( empty( $this->_settings[ $key ])) {
+		foreach ( $this->_providers[ $provider ]['map'] as $key => $value ) {
+			if ( $options[ $key ] === null ) {
 				continue;
 			}
 
 			if ( is_array( $value )) {
 				$paramName = $value['key'];
-				$paramValue = $this->_settings[ $paramName ];
+				$paramValue = $options[ $paramName ];
 
 				if ( isset( $value['prefix'])) {
 					$paramValue = $value['prefix'] . $paramValue;
 				}
 			} else {
 				$paramName = $value;
-				$paramValue = $this->_settings[ $paramName ];
+				$paramValue = $options[ $key ];
 			}
 
 			$params[ $paramName ] = $paramValue;
