480462b6d2b50f1c4fd6aa9c595d4c2ea805b5c7
davide <davide@davide-laptop>
2015-11-09 19:04:38 +0100

    Fixed lib importer

--- importer.php
+++ importer.php
@@ -1,4 +1,4 @@
 <?php
 
-require_once "./lib/Multiplayer/Multiplayer.php";
-require_once "./lib/Multiplayer/Param.php";
+require_once "lib/Multiplayer/Multiplayer.php";
+require_once "lib/Multiplayer/Param.php";
